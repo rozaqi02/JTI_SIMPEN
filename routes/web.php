9356864eb4e3be5c80242a312dc281093ed2b5e0
rozaqi02 <rojaki1419@gmail.com>
2024-12-04 11:23:29 +0700

    penambahan di profile

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SidebarController;
 use App\Http\Controllers\TugasController;
 use App\Http\Controllers\RegistrationController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,20 +35,12 @@ Route::middleware(['auth'])->group(function () {
 
 Route::get('/sidebar', [SidebarController::class, 'getSidebar'])->name('sidebar');
 
-// Route untuk mengambil tugas
-Route::post('/tugas/{tugas_id}/ambil', [TugasController::class, 'ambilTugas'])->name('tugas.ambil');
-
-//    Route::get('/dashboard', [WelcomeController::class, 'index']);
-
-        Route::get('/user', [UserController::class, 'index']);                          //menampilkan laman awal user
-        Route::post('/user/list', [UserController::class, 'list']);                      //menampilkan data user dalam bentuk json untuk datatable
-        Route::post('/ajax', [UserController::class, 'store_ajax']);                //menyimpan data user baru AJAX                //menyimpan perubahan data user
-        Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);        //menampilkan form detil data user AJAX
-        Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);        //menampilkan laman form edit user AJAX
-        Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);    //menyimpan perubahan data user AJAX
-        Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);   //menampilkan form confirm hapus data user AJAX
-        Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); //menghapus data user AJAX
-        Route::delete('/{id}', [UserController::class, 'destroy']);                 //menghapus data user
-        Route::get('/import', [UserController::class, 'import']);                   //menampilkan form impor data user
+Route::middleware(['authorize:ADM,MHS,TDK,DSN'])->group(function(){
+        Route::get('/profile', [ProfileController::class, 'index']);
+        Route::get('/profile/{id}/edit_ajax', [ProfileController::class, 'edit_ajax']);
+        Route::put('/profile/{id}/update_ajax', [ProfileController::class, 'update_ajax']);
+        Route::get('/profile/{id}/edit_foto', [ProfileController::class, 'edit_foto']);
+        Route::put('/profile/{id}/update_foto', [ProfileController::class, 'update_foto']);
+    });
 
 });
